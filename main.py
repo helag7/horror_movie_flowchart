9b90499f1fdbfafea95d2b64714b776bfbc9ed98
gardeniagerda <cieplagardenia@interia.pl>
2024-12-17 02:14:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,35 @@ import tkinter as tk
 def initialize_clips_environment():
     print("Initializing clips environment...")
     env = clips.Environment()
+       
+    try:
+        with open("templates.clp", "r") as file:
+            templates = file.read().strip().split("\n\n")
+            for temp in templates:
+                if temp.strip():
+                    env.build(temp)
+    except clips.CLIPSError as e:
+        print(f"Error in templates.clp: {e}")
+        return None
 
-    #Load template:
-    with open("templates.clp", "r") as file:
-        env.build(file.read())
-
-
-    #Load the rules:
     try:
         with open("rules.clp", "r") as file:
-            rules = file.read().strip().split("\n\n") #Split rules by empty lines
+            rules = file.read().strip().split("\n\n")
             for rule in rules:
                 if rule.strip():
                     env.build(rule)
     except clips.CLIPSError as e:
         print(f"Error in rules.clp: {e}")
+        return None
+    
+    try:
+        with open("horror_movies.clp", "r") as file:
+            horrors = file.read().strip().split("\n\n") 
+            for horror in horrors:
+                if horror.strip():
+                    env.build(horror)
+    except clips.CLIPSError as e:
+        print(f"Error in horror_movies.clp: {e}")
         return None
     
     env.run()
